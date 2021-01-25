4ef210cb86a1c8a420b75a0dd7322228651d4f4d
NVEtudiantOC <70369695+NVEtudiantOC@users.noreply.github.com>
2021-01-25 09:36:53 +0100

    Update backup.py

--- backup.py
+++ backup.py
@@ -31,10 +31,12 @@ def chargement_config(fichier_conf):
 ##### Fonctions sauvegarde et restauration bdd ####
 
 def sauvegarde_db(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME,TODAYDATE):
+    serveur_sauv = config['conf']['backup']['ip_sauv_dist']
     print ("Debut de la sauvegarde de la base de donnees " + DB_NAME)
     fichier_sauvegarde = str(DB_BACKUP) + TODAYDATE + DB_NAME
     #dumpcmd = "mysqldump -h " + DB_HOST + " -u " + DB_USER + " -p" + str(DB_PASSWORD) + " " + DB_NAME + ' > ' + fichier_sauvegarde + ".sql"
     dumpfile = fichier_sauvegarde + ".sql"
+    upfile_db = dumpfile + ".gz"
     tmpfile = open(dumpfile,'w')
     try:
     	subprocess.run(["mysqldump","--host=" + DB_HOST, "--user=" + DB_USER, "--password=" + str(DB_PASSWORD), DB_NAME], stdout=tmpfile)
@@ -45,7 +47,11 @@ def sauvegarde_db(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME,TODAYDATE):
     #gzipcmd = "gzip " + fichier_sauvegarde + ".sql"
     print ("Sauvegarde terminée\n")
     print ("La sauvegarde a été créée dans le fichier '" + fichier_sauvegarde + ".sql.gz'\n")
+    #subprocess.run(["scp", upfile_db, "administrateur@192.168.0.3:~/sauvegarde_db"])
+    #subprocess.run(["scp", upfile_db, DB_USER + "@192.168.0.3:" + DB_BACKUP])
+    subprocess.run(["scp", upfile_db, DB_USER + "@" + str(serveur_sauv) +":" + DB_BACKUP])
     return(0)
+
 
 def restaure_db(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME,TODAYDATE) -> None:
 	for delta in range(8):
@@ -55,9 +61,9 @@ def restaure_db(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME,TODAYDATE) -> None:
 			print("Une sauvegarde située dans'" + DB_BACKUP + "' a été trouvé!")
 			print ("La sauvegarde la plus récente est : '" + sauvegarde + "'")
 			try:
-				unzip = subprocess.Popen(['gzip', '-d', sauvegarde], stdout=subprocess.PIPE)
+				gunzip = subprocess.Popen(['gunzip', '-c', sauvegarde], stdout=subprocess.PIPE)
 				mysqlcmd = ['mysql', '-u', DB_USER, '-p' + str(DB_PASSWORD), '-h', DB_HOST, DB_NAME]
-				process = subprocess.Popen(mysqlcmd, stdin=unzip.stdout)
+				process = subprocess.Popen(mysqlcmd, stdin=gunzip.stdout)
 				process.wait()				
 				print("La sauvegarde '" + sauvegarde + "' a été restaurée avec succes!\n")
 				break
@@ -70,6 +76,8 @@ def restaure_db(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME,TODAYDATE) -> None:
 ##### Fonctions sauvegarde et restauration www ####
 
 def sauvegarde_www(site,key,TODAYDATE) -> None:
+	serveur_sauv = config['conf']['backup']['ip_sauv_dist']
+	USER = site['sites'][key]['mysql']['user']
 	print("Compression du dossier web  (/var/www...) du " + str(key) + "...")
 	fichier_sauvegarde = str(BACKUP_DIR_WEB) + '/www' + '_' + str(key) + '_' + TODAYDATE + '.tar'
 	dossier_racine = site['sites'][key]['web']['racine']
@@ -81,7 +89,10 @@ def sauvegarde_www(site,key,TODAYDATE) -> None:
 		dossier_tar.close()
 		#os.system('tar -cf '+ fichier_sauvegarde + site['sites'][key]['web']['racine'])
 		#os.system('tar -cf '+ config['conf']['backup']['backup_dir_web'] + str(key) + '_' + TODAYDATE +'.tar ' + site['sites'][key]['web']['racine'])
-		print("La sauvegarde de '" + dossier_racine + "' a été effectuée avec succes dans '" + fichier_sauvegarde + "'")
+		print("La sauvegarde de '" + dossier_racine + "' a été effectuée avec succès dans '" + fichier_sauvegarde + "'")
+		#subprocess.run(["scp", fichier_sauvegarde, "administrateur@192.168.0.3:~/sauvegarde_web"])
+		subprocess.run(["scp", fichier_sauvegarde, str(USER) + "@" + str(serveur_sauv) +":" + BACKUP_DIR_WEB])
+		print ("Sauvegarde du Site Web " + fichier_sauvegarde + " sur serveur distant, éffectuée!")
 	except tarfile.TarError as e: #Classe de base pour toutes les tarfileexceptions
 		print('Erreur rencontrée lors de la sauvegarde des fichiers du site Web',e)
 
@@ -108,6 +119,59 @@ def restaure_www(site,key,TODAYDATE) -> None:
 			print("Restauration terminée!")
 		else:
 			print ("Fichier de sauvegarde '" + fichier_sauvegarde + "'à J-" + str(delta) + " n'existe pas!...")
+
+def purge_backup(config):
+	DB_BACKUP = config['conf']['backup']['dossier_sauv']
+	BACKUP_DIR_WEB = config['conf']['backup']['backup_dir_web']
+	retention = config['conf']['backup']['retention']
+	# Supprimer les anciens fichiers .sql.gz et garder les n plus récents
+	#DB_BACKUP = '/home/administrateur/sauvegarde_db/'
+	nombre_fichier_recent = int(retention)
+	#nombre_fichier_recent = 7
+	#nombre_fichier_recent = config['backup']['retention']
+	print("Dans le répertoire'" + DB_BACKUP + "'")
+	print("Et dans le répertoire'" + BACKUP_DIR_WEB + "'")
+	print("Uniquement '" + str(nombre_fichier_recent) + "' sauvegardes seront gardées!\n") 
+	os.chdir(DB_BACKUP)
+	fichiers = sorted(os.listdir(os.getcwd()), key=os.path.getmtime)
+	fichiers_dat = []
+	for element in fichiers:
+	    if element.endswith('.sql.gz'):
+	        fichiers_dat.append(element)
+	#print("Contenu de fichiers_dat:'" + str(fichiers_dat) + "'\n")
+
+	liste_nouveau = fichiers_dat[-nombre_fichier_recent:]
+	#print("Contenu de liste_nouveau:'" + str(liste_nouveau) + "'\n") 
+
+	file_to_delete = fichiers_dat
+	#print("Contenu de file_to_delete:'" + str(file_to_delete) + "'\n")
+
+	for r in liste_nouveau:
+	    file_to_delete.remove(r)
+	if file_to_delete:
+	    for i in file_to_delete:
+	        os.remove("{}".format(DB_BACKUP + '/' + i))
+	# Supprimer les anciens fichiers .tar et garde les n plus récents
+	#DB_BACKUP = '/home/administrateur/sauvegarde_db/'
+	os.chdir(BACKUP_DIR_WEB)
+	fichiers = sorted(os.listdir(os.getcwd()), key=os.path.getmtime)
+	fichiers_dat = []
+	for element in fichiers:
+	    if element.endswith('.tar'):
+	        fichiers_dat.append(element)
+	#print("Contenu de fichiers_dat:'" + str(fichiers_dat) + "'\n")
+
+	liste_nouveau = fichiers_dat[-nombre_fichier_recent:]
+	#print("Contenu de liste_nouveau:'" + str(liste_nouveau) + "'\n") 
+
+	file_to_delete = fichiers_dat
+	#print("Contenu de file_to_delete:'" + str(file_to_delete) + "'\n")
+
+	for r in liste_nouveau:
+	    file_to_delete.remove(r)
+	if file_to_delete:
+	    for i in file_to_delete:
+	        os.remove("{}".format(BACKUP_DIR_WEB + '/' + i))
 
 ##### Fonction gestion de l'action demandée par l'utilisateur ####
 
@@ -156,7 +220,9 @@ def action_choisie(choix, conf_backup, site) -> None:
         print("Restauration terminée!\n")
 
       elif choix == 5:
-        print("Menu > \n")
+        print("Menu > Purge des Sauvegardes\n")
+        purge_backup(config)
+        print("Purge des Sauvegardes terminée!\n")
         
       elif choix == 6:
         exit(2)
@@ -171,7 +237,7 @@ def menu(config):
     print("\t[2] => Restaurer les bases mysql")
     print("\t[3] => Sauvegarder des sites web")
     print("\t[4] => Restaurer des sites web(root)")
-    print("\t[5] => ")
+    print("\t[5] => Purger les Sauvegardes")
     print("\n\t[6] => Exit")
     while True:
         try:
@@ -180,6 +246,7 @@ def menu(config):
         except ValueError:
             sys.stderr.write("{}\nErreur: choix non défini\n\n{}")
     action_choisie(choix, conf_backup, conf_backup)
+
 
 ##### MAIN ####
 
